cc4d4fd77a70922273177bafad9ef564a5dff738
blake-ros <dev@MSI.localdomain>
2021-02-23 14:45:06 -0800

    trying to figure out puppeteer packages

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
+
+(async function () {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--no-sandbox"]
+  });
+  const page = await browswer.newPage();
+  await page.goto("https://pptr.dev");
+
+  await page.waitFor(5000);
+  await browswer.close();
+})();
